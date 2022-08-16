8844dba139f2a9b423ab5e403b3d1d9b331ec89d
carlosmsx <carlosmsx@gmail.com>
2022-08-15 22:17:38 -0300

    proyecto creado con plantilla create-react-app, eliminados archivos innecesarios, modificado readme, agregado componente App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Paleta de Colores</h1>
+            <h2>Ejercicio 6</h2>
+        </div>
+    );
+};
+
+export default App;
